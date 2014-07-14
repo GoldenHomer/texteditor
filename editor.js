ec3d4e764f3c915dd2c72cc9409f87a35c2b217c
Jose Contreras <jocontre@joselcontreras.com>
2014-07-13 22:05:43 -0700

    Making progress

--- editor.js
+++ editor.js
@@ -1,13 +1,13 @@
-//JS execCommand
+//JS execCommand. 2nd parameter is to show a UI; 3rd is for an extra value argument (null since no such argument is needed)
 
-function bold (argument) {
-	// body...
+function bold () {
+	text1.document.execCommand("bold",false,null);
 }
 
 function italic (){
-
+	text1.document.execCommand("italic",false,null);
 }
 
-function underline (argument) {
-	
+function underline () {
+	text1.document.execCommand("underline",false,null);
 }
